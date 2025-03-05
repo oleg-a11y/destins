425b76078c3b623230faf9cf0dc5bd09aec96391
onlydisne <onlydisne@mail.ru>
2025-03-05 20:42:37 +0400

    Добавил экран загрузки

--- src/scenes/loading.js
+++ src/scenes/loading.js
@@ -2,15 +2,29 @@ import { loadLobbyScene } from "./lobby.js";
 
 function loadLoadingScene(container) {
   container.innerHTML = `
-        <div id="loading-screen">
-            <h1>Загрузка...</h1>
-            <div class="loader"></div>
+    <div id="loading-screen">
+      <div class="loading">
+        <div class="logo"></div>
+        <div class="progress_bar">
+          <div class="progress"></div>
         </div>
-    `;
+      </div>
+    </div>`;
 
-  setTimeout(() => {
-    loadLobbyScene(container);
-  }, 3000);
+  const progressBar = document.querySelector(".progress");
+  let progress = 0;
+
+  function updateProgress() {
+    if (progress < 100) {
+      progress += 2;
+      progressBar.style.width = progress + "%";
+      setTimeout(updateProgress, 60);
+    } else {
+      loadLobbyScene(container);
+    }
+  }
+
+  updateProgress();
 }
 
 export { loadLoadingScene };
